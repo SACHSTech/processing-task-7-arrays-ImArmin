cff423ca41f316af71136986271771133075aabe
ArminAwista <75390942+ImArmin@users.noreply.github.com>
2022-01-25 21:45:47 +0000

    Snowfall

--- Sketch.java
+++ Sketch.java
@@ -7,12 +7,12 @@ public class Sketch extends PApplet {
   }
 
   float[] circleY = new float[50];
-  float[] snow = new float[50];
+  float[] snowfall = new float[50];
   float[] mousePositionsX = new float[25];
   float[] mousePositionsY = new float[25];
-  int index = 0;
   int speed = 2;
-  int num = 50;
+  int num = 25;
+  int index = 0;
 
   public void setup() {
     background(51, 51, 51);
@@ -21,7 +21,7 @@ public class Sketch extends PApplet {
       circleY[i] = random(height);
     }
     for (int i = 0; i < 50; i++) {
-      snow[i] = 5;
+      snowfall[i] = 5;
     }
   }
 
@@ -29,20 +29,20 @@ public class Sketch extends PApplet {
 	  background(51, 51, 51);
     
     for (int circleX = 0; circleX < 50; circleX++) {
-      ellipse(circleX*10, circleY[circleX], 10, 10);
+      ellipse(circleX*8, circleY[circleX], 8, 8);
     }
 
     for (int i = 0; i < 50; i++) {
       circleY[i] += speed;
       if (circleY[i] >= 400) {
         circleY[i] = 0;
-        snow[i] += 5;
+        snowfall[i] += 5;
       }
     }
     
     for (int i = 0; i < 50; i++) {
       fill(255, 255, 255);
-      rect(i*10, 400-snow[i], 10, 400);
+      rect(i*8, 400-snowfall[i], 8, 400);
     }
 
     mousePositionsX[index] = mouseX;
